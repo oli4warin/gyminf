f8b2e95e31ee5eea3605c00733bb4f00b6489106
Olivier Warin <wasto@gmx.ch>
2024-10-02 10:38:30 +0200

    above3

---
+++ pytamaro/above3.py
@@ -0,0 +1,10 @@
+from pytamaro import above
+
+def above3(top, middle, bottom):
+    return above(
+            above(
+                top,
+                middle
+            ),
+            bottom
+    )
