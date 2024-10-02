a63703d66732d1ffdafb6d2d334900fb9e2ee020
Olivier Warin <wasto@gmx.ch>
2024-10-02 10:19:13 +0200

    solution

---
+++ pytamaro/three-ellipses1.py
@@ -0,0 +1,7 @@
+beside(
+    beside(
+            ellipse(40, 80, black),
+            ellipse(40, 40, black)
+    ),
+    ellipse(80,40,black)
+)
---
+++ pytamaro/three-ellipses2.py
@@ -0,0 +1,7 @@
+beside(
+    ellipse(40, 80, black),
+    beside(
+        ellipse(80,40,black),
+        ellipse(40, 40, black)
+    )
+)
