4ec238e705648b14f83b4b882df9b77d45bab795
Olivier Warin <wasto@gmx.ch>
2024-10-04 20:33:15 +0200

    alternating colors

--- pytamaro/alt-colors.py
+++ pytamaro/alt-colors.py
@@ -1,12 +1,11 @@
 from pytamaro import show_graphic, blue, red, ellipse, empty_graphic, beside
 
 my_graphic=empty_graphic()
-diameter_list=range(10,150,10)
 
-for index in range(len(diameter_list)):
+for diameter in range(10,150,10):
     my_graphic = beside(my_graphic, 
-            ellipse(diameter_list[index], diameter_list[index],
-                blue if index%2==0 else red
+            ellipse(diameter, diameter,
+                blue if diameter%20==0 else red
             )
     )
 
