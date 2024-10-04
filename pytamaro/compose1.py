d92c0340c37c88978f6ffad0039db828ff0c34f7
Olivier Warin <wasto@gmx.ch>
2024-10-04 17:41:42 +0200

    added solutions

--- pytamaro/compose1.py
+++ pytamaro/compose1.py
@@ -1,9 +1,9 @@
-from pytamaro import compose, pin, show_graphic, save_graphic, rectangle, top_left, top_center, blue, red, top_right, center, bottom_center, bottom_left, bottom_right, center_left
+from pytamaro import compose, pin, show_graphic, save_graphic, rectangle, top_left, top_center, blue, red, top_right, center, bottom_center, bottom_left, bottom_right, center_left, green
 
 def wide():
     return rectangle(200, 80, blue)
 def tall():
-    return rectangle(80, 200, red)
+    return rectangle(80, 200, green)
 
 save_graphic(
         "../../../images/pytamaro-compose-example1.svg",
@@ -39,3 +39,35 @@ save_graphic(
 )
 
 
+save_graphic(
+        "../../../images/pytamaro-compose-example1-pin.svg",
+        compose(
+            pin(top_left, wide()),
+            pin(top_left, tall()),
+        ), True
+)
+
+
+save_graphic(
+        "../../../images/pytamaro-compose-example2-pin.svg",
+        compose(
+            pin(top_left, wide()),
+            pin(top_right, tall()),
+        ), True
+)
+
+save_graphic(
+        "../../../images/pytamaro-compose-example3-pin.svg",
+        compose(
+            pin(bottom_right, wide()),
+            pin(top_center, tall()),
+        ), True
+)
+
+save_graphic(
+        "../../../images/pytamaro-compose-example4-pin.svg",
+        compose(
+            pin(top_right, wide()),
+            pin(center_left, tall()),
+        ), True
+)
