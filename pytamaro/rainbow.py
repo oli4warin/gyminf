402f42b25ac8489cd7ed4da708a62da5b4b51fe2
Olivier Warin <wasto@gmx.ch>
2024-10-04 18:27:17 +0200

    a rainbow

---
+++ pytamaro/rainbow.py
@@ -0,0 +1,20 @@
+from pytamaro import rgb_color, white, empty_graphic, circular_sector, show_graphic, pin, compose, bottom_center
+
+rainbow_red = rgb_color(255, 0, 0) 
+rainbow_orange = rgb_color(255, 140, 0) 
+rainbow_yellow = rgb_color(255, 240, 0) 
+rainbow_green = rgb_color(0, 138, 40) 
+rainbow_blue = rgb_color(0, 80, 255) 
+rainbow_purple = rgb_color(120, 0, 140) 
+
+color_list=[rainbow_red, rainbow_orange, rainbow_yellow, rainbow_green, rainbow_blue, rainbow_purple, white]
+radius_list=range(200, 60, -20)
+
+rainbow = empty_graphic()
+
+for index in range(len(color_list)):
+    rainbow = pin(bottom_center,compose(circular_sector(radius_list[index],180,color_list[index]),rainbow))
+
+show_graphic(rainbow)
+
+
