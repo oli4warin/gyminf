d7c0a60f4f8352b2c8f8758a8e1a5a654b20a1c6
Olivier Warin <wasto@gmx.ch>
2024-10-04 18:40:52 +0200

    An example for the range command

---
+++ pytamaro/range-example.py
@@ -0,0 +1,10 @@
+from pytamaro import show_graphic, blue, ellipse, empty_graphic, beside
+
+my_graphic=empty_graphic()
+
+for diameter in range(10,150,10):
+    my_graphic = beside(my_graphic, ellipse(diameter,diameter,blue))
+
+show_graphic(my_graphic)
+
+
