011373bbbfe1cfe2ec2f3d8eed74b81337f397b0
Olivier Warin <wasto@gmx.ch>
2024-10-06 21:06:07 +0200

    a static pacman

---
+++ pytamaro/static-pacman.py
@@ -0,0 +1,8 @@
+from pytamaro import circular_sector, yellow, rotate, show_graphic, save_graphic
+
+show_graphic(
+    rotate(
+            22.5,
+            circular_sector(100,360-45,yellow)
+    )
+)
