c49153b54889515c074c139455e8912718742f69
Olivier Warin <wasto@gmx.ch>
2025-02-19 11:48:48 +0100

    outsourced save_graphic commands

--- pytamaro/pin-compose-example.py
+++ pytamaro/pin-compose-example.py
@@ -7,10 +7,3 @@ compose(
             ),
         ellipse(50,50,magenta)
 )
-
-save_graphic("../../../images/pytamaro-pincompose-triangle.svg", triangle(100,100,60,blue), True)
-save_graphic("../../../images/pytamaro-pincompose-triangle-pin.svg", pin(top_center,triangle(100,100,60,blue)), True)
-save_graphic("../../../images/pytamaro-pincompose-circle.svg", ellipse(50,50,magenta), True)
-save_graphic("../../../images/pytamaro-pincompose-composed.svg", compose( pin(top_center,triangle(100, 100, 60, blue)), ellipse(50,50,magenta)), True)
-save_graphic("../../../images/pytamaro-pincompose-example.svg", compose( pin(top_center,triangle(100, 100, 60, blue)), ellipse(50,50,magenta)))
-
