069c53872392b9dfc478c5fa22314fee047aca8f
Olivier Warin <wasto@gmx.ch>
2025-02-21 13:43:13 +0100

    fixed very wrong syntax

--- pytamaro/my-beside.py
+++ pytamaro/my-beside.py
@@ -1,8 +1,9 @@
 from pytamaro import pin, compose, center, center_left, center_right
 
-def my_beside(left: Graphic, right: Graphic) -> Graphic {
+def my_beside(left: Graphic, right: Graphic) -> Graphic:
     return pin(center,
-               pin(center_right, left),
-               pin(center_left, right)
+        compose(
+            pin(center_right, left),
+            pin(center_left, right)
+        )
     )
-}
