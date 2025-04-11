9229c36d51eb7ab9ba2b75e41939a9917cc4315f
Olivier Warin <wasto@gmx.ch>
2025-04-11 20:57:41 +0200

    using 2**0.5 for sqrt(2)

--- pytamaro/fractal2.py
+++ pytamaro/fractal2.py
@@ -2,25 +2,14 @@ from pytamaro import rectangle, black, show_graphic, overlay, rotate, white
 
 def square(size: float):
     return overlay(
-        rectangle(
-            size-10,
-            size-10,
-            white,
-        ),
-        rectangle(
-            size,
-            size,
-            black
-        )
-    )
+        rectangle(size-10, size-10, white),
+        rectangle(size, size, black))
 
 def fractal(size: float):
     if size<20:
         return square(size)
     else: return overlay(
-            rotate(45,
-                   fractal(size/1.414214)
-            ),
+            rotate(45, fractal(size/2**0.5)),
             square(size)
         )
 
